d6b1eef2f8b01936e2f41482469ecd0da40cb54b
Gauravjot Garaya <gauravjot@outlook.com>
2023-06-12 23:41:11 -0700

    fixed router issue

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 	<React.StrictMode>
-		<Router>
+		<Router basename={"/redditwal"} history={history}>
 			<Routes>
 				<Route path="/" element={<App />} />
 			</Routes>
