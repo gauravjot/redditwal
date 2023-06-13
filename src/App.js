bdf19bea98f3de167639cda05ed8cb2146f210da
Gauravjot Garaya <gauravjot@outlook.com>
2023-06-12 23:26:54 -0700

    added router, made top bar sticky

--- src/App.js
+++ src/App.js
@@ -6,150 +6,187 @@ import "./App.css";
 import WallpaperCard from "./components/WallpaperCard";
 
 function App() {
-  const wallpaperSubs = "wallpapers+wallpaper+widescreenwallpaper";
-  const baseURL = "https://www.reddit.com/r/";
-  const sorts = ["hot", "new", "top", "controversial", "rising"];
+	const wallpaperSubs = "wallpapers+wallpaper+widescreenwallpaper+WQHD_Wallpaper";
+	const baseURL = "https://www.reddit.com/r/";
 
-  const [currentSub, setCurrentSub] = React.useState(wallpaperSubs);
-  const [currentSort, setCurrentSort] = React.useState("hot");
-  const [nsfwFilter, setNsfwFilter] = React.useState(true);
+	const [currentSub, setCurrentSub] = React.useState(wallpaperSubs);
+	const currentSort = "hot";
+	const [nsfwFilter, setNsfwFilter] = React.useState(false);
 
-  const [data, setData] = React.useState([]);
-  const [isErrorResponse, setIsErrorResponse] = React.useState(false);
+	const [data, setData] = React.useState([]);
+	const [isErrorResponse, setIsErrorResponse] = React.useState(false);
+	let settingsRef = React.useRef(null);
 
-  React.useEffect(() => {
-    axios
-      .get(baseURL + currentSub + "/" + currentSort + ".json?limit=1000")
-      .then((response) => {
-        setData(response.data.data.children);
-        if (!isErrorResponse) {
-          setIsErrorResponse(false);
-        }
-      })
-      .catch((err) => {
-        setIsErrorResponse(true);
-      });
-  }, [currentSort]);
+	React.useEffect(() => {
+		setIsErrorResponse(false);
+		setData([]);
+		axios
+			.get(baseURL + currentSub + "/" + currentSort + ".json?limit=1000")
+			.then((response) => {
+				setData(response.data.data.children);
+			})
+			.catch((err) => {
+				setIsErrorResponse(true);
+			});
+	}, [currentSub]);
 
-  function subChange(sub) {
-    if (sub === "" || sub === undefined) {
-      sub = wallpaperSubs;
-    }
-    setData([]);
-    setIsErrorResponse(false);
-    axios
-      .get(baseURL + sub + "/" + currentSort + ".json?limit=100")
-      .then((response) => {
-        setCurrentSub(sub);
-        setData(response.data.data.children);
-        if (!isErrorResponse) {
-          setIsErrorResponse(false);
-        }
-      })
-      .catch((err) => {
-        setIsErrorResponse(true);
-      });
-  }
+	function subChange(sub) {
+		if (sub.length < 1) {
+			setCurrentSub(wallpaperSubs);
+			return;
+		}
+		if (sub.length < 3) {
+			return;
+		}
+		setCurrentSub(sub);
+	}
 
-  const _subChange = _.debounce((sub) => subChange(sub), 600);
+	const _subChange = _.debounce((sub) => subChange(sub), 600);
 
-  function aspectClassSwitch(width, height) {
-    let a_ratio = width / height;
-    switch (true) {
-      case a_ratio > 3.5:
-        return "card grid-span-3";
-      case a_ratio > 2.2:
-        return "card grid-span-2";
-      case a_ratio > 0.8:
-        return "card";
-      case a_ratio > 0.4:
-        return "card grid-row-span-2";
-      default:
-        return "d-none";
-    }
-  }
+	function aspectClassSwitch(width, height) {
+		let a_ratio = width / height;
+		switch (true) {
+			case a_ratio > 3.5:
+				return "card grid-span-3";
+			case a_ratio > 2.2:
+				return "card grid-span-2";
+			case a_ratio > 0.8:
+				return "card";
+			case a_ratio > 0.4:
+				return "card grid-row-span-2";
+			default:
+				return "d-none";
+		}
+	}
 
-  return (
-    <section className="container">
-      <div className="searchbar">
-        <input
-          type="text"
-          name="subreddit"
-          placeholder="start typing a subreddit..."
-          onChange={(event) => _subChange(event.target.value.trim())}
-        />
-        <button className="btnSettings">
-          <i
-            className="fas fa-cog"
-            onClick={() => {
-              document.getElementById("options").style.visibility =
-                document.getElementById("options").style.visibility ===
-                "visible"
-                  ? "hidden"
-                  : "visible";
-            }}
-          ></i>
-          <div className="options" id="options">
-            <div>
-              <i
-                className={
-                  nsfwFilter
-                    ? "fas fa-check-circle green"
-                    : "fas fa-times-circle red"
-                }
-              ></i>
-              <input
-                type="checkbox"
-                onClick={() => {
-                  setNsfwFilter(document.getElementById("nsfwFilter").checked);
-                }}
-                id="nsfwFilter"
-                checked={nsfwFilter}
-              />
-              <label for="nsfwFilter">NSFW Filter</label>
-            </div>
-          </div>
-        </button>
-      </div>
-      <div className="wallpapers">
-        {data.length > 0 ? (
-          data.map((item) =>
-            item.data.preview ? (
-              item.data.preview.enabled &&
-              item.data.preview.images[0].resolutions[
-                item.data.preview.images[0].resolutions.length - 1
-              ] ? (
-                <div
-                  key={item.data.id}
-                  className={aspectClassSwitch(
-                    item.data.preview.images[0].source.width,
-                    item.data.preview.images[0].source.height
-                  )}
-                >
-                  <WallpaperCard
-                    baseURL={baseURL}
-                    wallpaper={item.data}
-                    nsfwFilter={nsfwFilter}
-                  />
-                </div>
-              ) : (
-                ""
-              )
-            ) : (
-              ""
-            )
-          )
-        ) : !isErrorResponse ? (
-          <div class="lds-dual-ring"></div>
-        ) : (
-          <div className="error">
-            <i className="fas fa-exclamation-triangle"></i>
-            <div>Network Error or Sub does not exist :/</div>
-          </div>
-        )}
-      </div>
-    </section>
-  );
+	const toggleEventHandler = React.useCallback((e) => {
+		/* useCallback so function doesnt change in re-renders
+       otherwise our add/remove eventListeners will go haywire */
+		/* whitelisted with if-else loop:
+		   1. The menu box
+		   2. The button which is toggle
+		   3. Close-icon in Multiselect
+		   add more for exceptions */
+		if (
+			!document.getElementById("settings-menu")?.contains(e.target) &&
+			!settingsRef.current.contains(e.target)
+		) {
+			toggleSettings();
+		}
+	}, []);
+
+	/* Filter Toggle */
+	const toggleSettings = () => {
+		if (settingsRef.current) {
+			let attribValue = settingsRef.current.getAttribute("aria-hidden");
+			settingsRef.current.setAttribute(
+				"aria-hidden",
+				attribValue === "true" ? "false" : "true"
+			);
+			if (attribValue === "true" ? false : true) {
+				window.removeEventListener("click", toggleEventHandler);
+			} else {
+				// Detect clicks outside of filter box
+				window.addEventListener("click", toggleEventHandler);
+			}
+		}
+	};
+
+	let prevScrollpos = window.pageYOffset;
+	window.onscroll = function () {
+		let currentScrollPos = window.pageYOffset;
+		if (currentScrollPos < 200 || prevScrollpos > currentScrollPos) {
+			document.getElementById("nav-bar").style.top = "0";
+		} else {
+			settingsRef.current.setAttribute("aria-hidden", "true");
+			window.removeEventListener("click", toggleEventHandler);
+			document.getElementById("nav-bar").style.top = "-6rem";
+		}
+		prevScrollpos = currentScrollPos;
+	};
+
+	return (
+		<section className="container">
+			<div className="searchbar" id="nav-bar">
+				<div className="container flex">
+					<input
+						type="text"
+						name="subreddit"
+						placeholder="start typing a subreddit..."
+						onChange={(event) => _subChange(event.target.value.trim())}
+					/>
+					<div className="dropdown" id="settings-menu">
+						<button
+							className="btnSettings"
+							onClick={() => {
+								toggleSettings();
+							}}
+						>
+							<span className="ic ic-settings invert-1"></span>
+						</button>
+						<div
+							className="menu"
+							id="options"
+							aria-hidden="true"
+							ref={settingsRef}
+						>
+							<button
+								className="menu-item"
+								onClick={() => {
+									setNsfwFilter(!nsfwFilter);
+								}}
+							>
+								<span
+									className={
+										(nsfwFilter ? "ic-checked" : "ic-unchecked") +
+										" ic-md invert-1"
+									}
+								></span>
+								<label htmlFor="nsfwFilter">Enable NSFW</label>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="wallpapers">
+				{data.length > 0 ? (
+					data.map((item) =>
+						item.data.preview ? (
+							item.data.preview.enabled &&
+							item.data.preview.images[0].resolutions[
+								item.data.preview.images[0].resolutions.length - 1
+							] ? (
+								<div
+									key={item.data.id}
+									className={aspectClassSwitch(
+										item.data.preview.images[0].source.width,
+										item.data.preview.images[0].source.height
+									)}
+								>
+									<WallpaperCard
+										baseURL={baseURL}
+										wallpaper={item.data}
+										nsfwFilter={nsfwFilter}
+									/>
+								</div>
+							) : (
+								""
+							)
+						) : (
+							""
+						)
+					)
+				) : !isErrorResponse ? (
+					<div className="lds-dual-ring"></div>
+				) : (
+					<div className="error">
+						<i className="fas fa-exclamation-triangle"></i>
+						<div>Network Error or Sub does not exist :/</div>
+					</div>
+				)}
+			</div>
+		</section>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+	<React.StrictMode>
+		<Router>
+			<Routes>
+				<Route path="/" element={<App />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>
 );
